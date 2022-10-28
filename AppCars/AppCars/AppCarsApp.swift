9caf0352b401fda0baa13a2bfb9155af520c2e63
Pedro Boga <45229010+pedroboga@users.noreply.github.com>
2022-10-28 14:58:55 -0300

    Created project

---
+++ AppCars/AppCars/AppCarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppCarsApp.swift
+//  AppCars
+//
+//  Created by Pedro Boga on 28/10/22.
+//
+
+import SwiftUI
+
+@main
+struct AppCarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
