228a6607359b610f49b06ecb0e2c2a75d9e9a775
Pedro Boga <45229010+pedroboga@users.noreply.github.com>
2022-10-31 14:09:14 -0300

    Created login/logout flow

--- AppCars/AppCars/AppCarsApp.swift
+++ AppCars/AppCars/AppCarsApp.swift
@@ -9,8 +9,21 @@ import SwiftUI
 
 @main
 struct AppCarsApp: App {
+    
     var body: some Scene {
         WindowGroup {
+            AppLogger()
+        }
+    }
+}
+
+struct AppLogger: View {
+    @AppStorage("isLoggedIn", store: .standard) var isLoggedIn: Bool = false
+    
+    var body: some View {
+        if isLoggedIn {
+            AppTabView()
+        } else {
             LoginView()
         }
     }
